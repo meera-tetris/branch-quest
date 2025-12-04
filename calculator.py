b5076890a950a3eff34d01ce499e2723b118686e
meera-tetris <meera@tetrisinnovations.com>
2025-12-04 11:39:37 +0530

    Update and rename recipes.md to calculator.py

---
+++ calculator.py
@@ -0,0 +1,29 @@
+'''
+Add functions for add, subtract, multiply and divide
+Example:
+add: num1 + num2
+sub: num1 - num2
+mul: num1 * num2
+div: num1 / num2
+'''
+
+def add(a, b):
+  pass
+
+def sub(a, b):
+  pass
+
+def mul(a, b):
+  pass
+
+def div(a, b):
+  pass
+
+def main():
+  print(add(10, 5))
+  print(sub(10, 5))
+  print(mul(10, 5))
+  print(div(10, 5))
+
+if __name__ == "__main__":
+  main()
--- recipes.md
+++
@@ -1,7 +0,0 @@
-### Task 1: Saathvika
-
-### Task 2: Sakshi
-
-### Task 3: Vishnu
-
-### Task 4: Rajnish
