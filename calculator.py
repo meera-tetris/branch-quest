57ef764e57a0b51c587a350827a50955a5eb1573
Sakshishettyp <sakshi@tetrisinnovations.com>
2025-12-04 15:05:46 +0530

    added multiplication logic

--- calculator.py
+++ calculator.py
@@ -1,29 +1,35 @@
-'''
+"""
 Add functions for add, subtract, multiply and divide
 Example:
 add: num1 + num2
 sub: num1 - num2
 mul: num1 * num2
 div: num1 / num2
-'''
+"""
+
 
 def add(a, b):
-  pass
+    pass
+
 
 def sub(a, b):
-  pass
+    pass
+
 
 def mul(a, b):
-  pass
+    return a * b
+
 
 def div(a, b):
-  pass
+    pass
+
 
 def main():
-  print(add(10, 5))
-  print(sub(10, 5))
-  print(mul(10, 5))
-  print(div(10, 5))
+    print(add(10, 5))
+    print(sub(10, 5))
+    print(mul(10, 5))
+    print(div(10, 5))
+
 
 if __name__ == "__main__":
-  main()
+    main()
